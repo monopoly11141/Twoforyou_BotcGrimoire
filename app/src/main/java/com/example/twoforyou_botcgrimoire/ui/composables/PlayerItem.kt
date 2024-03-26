cc89fe217e38d90a48bfd5f1d9016adb1b9942bc
monopoly1114 <31841074+monopoly1114@users.noreply.github.com>
2024-03-26 17:27:43 +0900

    Allow dragging and zooming

--- app/src/main/java/com/example/twoforyou_botcgrimoire/ui/composables/PlayerItem.kt
+++ app/src/main/java/com/example/twoforyou_botcgrimoire/ui/composables/PlayerItem.kt
@@ -1,39 +1,61 @@
 package com.example.twoforyou_botcgrimoire.ui.composables
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.twoforyou_botcgrimoire.domain.models.Player
 
 @Composable
 fun PlayerItem(
-    player : Player
+    player: Player,
+    modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentSize(Alignment.Center)) {
-        Box(
-            modifier = Modifier
-                .size(100.dp)
-                .clip(CircleShape)
-                .background(Color.Gray)
-        ) {
-           AsyncImage(
-               model = player.character?.imageUrl,
-               contentDescription = "Image of ${player.character?.name ?: "NO_CHARACTER"}"
-           )
-        }
+    var density = LocalDensity.current
+
+    var offsetX by remember { mutableFloatStateOf(0f) }
+    var offsetY by remember { mutableFloatStateOf(0f) }
+
+    Box(
+        modifier = modifier
+            .offset(
+                (offsetX / density.density).dp,
+                (offsetY / density.density).dp,
+            )
+            .size(100.dp)
+            .clip(CircleShape)
+            .background(Color.Gray)
+            .pointerInput(Unit) {
+                detectDragGestures { change, dragAmount ->
+                    change.consume()
+                    offsetX += dragAmount.x
+                    offsetY += dragAmount.y
+                }
+            }
+    ) {
+        AsyncImage(
+            model = player.character?.imageUrl,
+            contentDescription = "Image of ${player.character?.name ?: "NO_CHARACTER"}"
+        )
     }
+
 }
--- app/src/main/java/com/example/twoforyou_botcgrimoire/ui/screen/grimoire/GrimoireScreen.kt
+++ app/src/main/java/com/example/twoforyou_botcgrimoire/ui/screen/grimoire/GrimoireScreen.kt
@@ -1,17 +1,53 @@
 package com.example.twoforyou_botcgrimoire.ui.screen.grimoire
 
+import android.content.res.Configuration
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.TransformableState
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
+import coil.size.Size
+import com.example.twoforyou_botcgrimoire.R
 import com.example.twoforyou_botcgrimoire.domain.models.CharacterList
 import com.example.twoforyou_botcgrimoire.domain.models.Player
+import com.example.twoforyou_botcgrimoire.ui.composables.CharacterItem
 import com.example.twoforyou_botcgrimoire.ui.composables.PlayerItem
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun GrimoireScreen(
     characterList: CharacterList,
@@ -20,16 +56,49 @@ fun GrimoireScreen(
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
     viewModel.updateInPlayCharacters(characterList.inPlayCharacters)
-    Column(
 
-    ) {
-        for(inPlayCharacter in state.inPlayCharacters) {
-            PlayerItem(
-                player = Player(
-                    character = inPlayCharacter
-                )
-            )
+    var zoom by remember { mutableStateOf(1f) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
+
+    Box(
+        Modifier
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                //zoom in/out and move around
+                detectTransformGestures { gestureCentroid, gesturePan, gestureZoom, _ ->
+                    val oldScale = zoom
+                    val newScale = (zoom * gestureZoom).coerceIn(0.25f..3f)
+                    offset =
+                        (offset + gestureCentroid / oldScale) - (gestureCentroid / newScale + gesturePan / oldScale)
+                    zoom = newScale
+                }
+            }) {
+        Box(
+            Modifier
+
+                .graphicsLayer {
+                    translationX = -offset.x * zoom
+                    translationY = -offset.y * zoom
+                    scaleX = zoom
+                    scaleY = zoom
+                }
+
+        ) {
+            FlowRow(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                for (inPlayCharacter in state.inPlayCharacters) {
+                    PlayerItem(
+                        player = Player(
+                            character = inPlayCharacter
+                        ),
+                        modifier = Modifier
+                    )
+                }
+            }
         }
     }
+
 
 }
